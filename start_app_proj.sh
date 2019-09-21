497e599bc719b721435a0a05f1db07cf44c5601c
wubw555 <wu555@wu555-MacBook.local>
2019-09-22 01:42:23 +0900

    add .gitignore

--- start_app_proj.sh
+++ start_app_proj.sh
@@ -5,6 +5,7 @@ git init
 git secrets --install
 git secrets --add 'private_key'
 git secrets --add 'private_key_id'
+touch .gitignore
 read -p "project_id: " project_id
 gcloud config set project $project_id
 read -p "std/flex: " sf
